5f463b672d2f9c847140e5a6016441f30986db0b
K.R Sahana <shettysahana170@gmail.com>
2024-08-14 11:09:57 +0530

    first commit

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "bdc39b35ddmsh5afb3da23285b81p105f6fjsncae821211065",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "f42c43224be975b5257ae7fbc17318c9"; 
